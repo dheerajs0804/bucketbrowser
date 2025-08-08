fdad7a6bcc1528645e838b244ff93c431a72a641
dheeraj <dheeraj2004.sharma@gmail.com>
2025-08-08 11:27:34 +0530

    Remove .next directory from repository

--- .next/static/chunks/src_app_page_tsx_495b09d2._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_d2414a59._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_b362cef4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_3f8483d6._.js",
-  "static/chunks/src_components_92c204fa._.js"
-],
-    source: "dynamic"
-});
